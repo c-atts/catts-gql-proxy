e1276d36b6bc9c58d817e77028a6014ad87c7dda
Kristofer <kristofer@fmckl.se>
2024-08-19 16:22:34 +0200

    Support REST queries

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+mod filter;
+mod header;
+mod proxy_request;
+mod url;
+
 use worker::*;
 
 #[event(fetch)]
@@ -8,106 +13,18 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> worker::Resu
             let mut headers = Headers::new();
             headers.append("Access-Control-Allow-Origin", "*")?;
             headers.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS")?;
-            headers.append(
-                "Access-Control-Allow-Headers",
-                "Content-Type, x-gql-query-url",
-            )?;
-            headers.append("Access-Control-Max-Age", "600")?; // 10 minutes
             Ok(Response::empty()?.with_headers(headers))
         })
-        .get_async("/:cache_key", |req, ctx| async move {
-            handle_graphql_request(req, ctx).await
+        .get_async("/:cache_key", |_, _| async move {
+            Response::error("Method not allowed", 405)
         })
         .post_async("/:cache_key", |req, ctx| async move {
-            handle_graphql_request(req, ctx).await
+            let namespace = ctx.durable_object("CATTS-QUERY-PROXY")?;
+            let url = req.url()?;
+            let url = url.as_str();
+            let stub = namespace.id_from_name(url)?.get_stub()?;
+            stub.fetch_with_request(req).await
         })
         .run(req, env)
         .await
-}
-
-pub async fn handle_graphql_request(
-    mut req: Request,
-    ctx: RouteContext<()>,
-) -> worker::Result<Response> {
-    match ctx.param("cache_key") {
-        Some(_) => (),
-        None => return Response::error("Parameter 'cache_key' is missing", 400),
-    };
-
-    let url = req.url()?;
-    let c = Cache::default();
-    let cached = c.get(url.as_str(), false).await?;
-    if let Some(response) = cached {
-        let mut headers = Headers::new();
-        headers.append("Access-Control-Allow-Origin", "*")?;
-        return Ok(response.with_headers(headers));
-    }
-
-    let headers = req.headers();
-    let query_url = match headers.get("x-gql-query-url") {
-        Ok(url) => match url {
-            Some(url) => url,
-            None => return Response::error("Header 'x-gql-query-url' is missing", 400),
-        },
-        Err(err) => return Response::error(err.to_string(), 400),
-    };
-    let query_url = match process_query_url(&query_url, &ctx) {
-        Ok(url) => url,
-        Err(err) => return Response::error(err.to_string(), 400),
-    };
-
-    let body = req.text().await?;
-
-    let mut headers = Headers::new();
-    headers.append("Content-Type", "application/json")?;
-    headers.append("User-Agent", "c-atts/0.0.1")?;
-
-    let mut init = RequestInit::new();
-    init.with_headers(headers);
-    init.with_method(Method::Post);
-    init.with_body(Some(body.into()));
-
-    let request = Request::new_with_init(&query_url, &init)?;
-    let response = Fetch::Request(request).send().await;
-
-    match response {
-        Ok(mut response) => {
-            let mut headers = Headers::new();
-            headers.append("Access-Control-Allow-Origin", "*")?;
-            headers.append("max-age", "600")?; // 10 minutes
-            let cloned_response = response.cloned()?;
-            c.put(url.as_str(), cloned_response.with_headers(headers.clone()))
-                .await?;
-            Ok(response.with_headers(headers))
-        }
-        Err(e) => Response::error(format!("Error fetching data: {}", e), 500),
-    }
-}
-
-fn process_query_url(url: &str, ctx: &RouteContext<()>) -> Result<String> {
-    let parsed_url = Url::parse(url)?;
-
-    let url = match parsed_url.domain() {
-        Some(domain) => {
-            if domain.ends_with("thegraph.com") {
-                process_the_graph_path(url, ctx)
-            } else {
-                return Ok(url.to_string());
-            }
-        }
-        None => return Err("Invalid domain".into()),
-    }?;
-
-    // Return the modified URL as a String
-    Ok(url)
-}
-
-fn process_the_graph_path(path: &str, ctx: &RouteContext<()>) -> Result<String> {
-    // Set this secret using `wrangler secret put THEGRAPH_API_KEY`
-    let api_key = match ctx.env.secret("THEGRAPH_API_KEY") {
-        Ok(key) => key,
-        Err(_) => return Err("THEGRAPH_API_KEY secret is missing".into()),
-    };
-
-    Ok(path.replace("{api-key}", &api_key.to_string()))
 }
---
+++ src/proxy_request.rs
@@ -0,0 +1,131 @@
+use futures_timer::Delay;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use serde_wasm_bindgen::to_value;
+use std::time::Duration;
+use wasm_bindgen::JsValue;
+use worker::*;
+
+use crate::{filter::filter_response_data, header::create_headers, url::create_url};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProxyRequestBody {
+    pub query: Option<String>,
+    pub variables: Option<Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProxyRequest {
+    pub url: String,
+    pub headers: Option<String>,
+    pub filter: Option<String>,
+    pub body: Option<ProxyRequestBody>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ProxyRequestState {
+    Idle,
+    InProgress,
+    Success,
+    Error,
+}
+
+#[durable_object]
+pub struct ProxyRequestDurableObject {
+    state: ProxyRequestState,
+    response_body: Option<Value>,
+    response_code: Option<u16>,
+    env: Env,
+}
+
+impl ProxyRequestDurableObject {
+    async fn fetch_query(&mut self, mut req: Request) -> std::result::Result<Value, String> {
+        self.state = ProxyRequestState::InProgress;
+
+        let request_body = req.text().await.map_err(|e| e.to_string())?;
+        let recipe_request: ProxyRequest =
+            serde_json::from_str(&request_body).map_err(|e| e.to_string())?;
+        let proxy_headers = create_headers(&recipe_request, &self.env)?;
+        let proxy_url = create_url(&recipe_request, &self.env);
+
+        let mut init = RequestInit::new();
+        init.with_headers(proxy_headers);
+
+        if let Some(body) = recipe_request.body {
+            init.with_method(Method::Post);
+            let body: JsValue = to_value(&body).unwrap();
+            init.with_body(Some(body));
+        } else {
+            init.with_method(Method::Get);
+        }
+
+        let request = Request::new_with_init(&proxy_url, &init).map_err(|e| e.to_string())?;
+        let response = Fetch::Request(request).send().await;
+
+        match response {
+            Ok(mut response) => {
+                let mut response_body = response.json().await.map_err(|e| e.to_string())?;
+
+                if let Some(filter) = &recipe_request.filter {
+                    response_body = filter_response_data(&response_body, filter)?;
+                }
+
+                Ok(response_body)
+            }
+            Err(e) => Err(format!("Error fetching data: {}", e)),
+        }
+    }
+}
+
+#[durable_object]
+impl DurableObject for ProxyRequestDurableObject {
+    fn new(state: State, env: Env) -> Self {
+        Self {
+            state: ProxyRequestState::Idle,
+            response_body: None,
+            response_code: None,
+            env,
+        }
+    }
+
+    async fn fetch(&mut self, req: Request) -> Result<Response> {
+        if self.state == ProxyRequestState::Idle {
+            let query_result = self.fetch_query(req).await;
+            match query_result {
+                Ok(response_body) => {
+                    self.state = ProxyRequestState::Success;
+                    self.response_body = Some(response_body);
+                }
+                Err(e) => {
+                    self.state = ProxyRequestState::Error;
+                    self.response_code = Some(400);
+                    self.response_body = Some(json!({
+                        "message": e,
+                    }));
+                }
+            }
+        };
+
+        if self.state == ProxyRequestState::InProgress {
+            Delay::new(Duration::from_millis(100)).await;
+        };
+
+        match &self.response_body {
+            Some(response_body) => {
+                let response = Response::from_json(&response_body.clone())?;
+
+                let mut headers = Headers::new();
+                headers.append("Access-Control-Allow-Origin", "*")?;
+
+                match &self.state {
+                    ProxyRequestState::Success => {
+                        Ok(response.with_headers(headers).with_status(200))
+                    }
+                    ProxyRequestState::Error => Ok(response.with_headers(headers).with_status(400)),
+                    _ => unreachable!(),
+                }
+            }
+            None => Response::error("Internal error", 500),
+        }
+    }
+}
