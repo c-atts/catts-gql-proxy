c44aef5378c1fab6a48f8a3398ca6ea07424e691
Kristofer <kristofer@fmckl.se>
2024-08-20 13:06:06 +0200

    examples

---
+++ examples/the-graph.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Example query that fetches the number of votes for a specific delegate from the ENS Governance subgraph on Arbitrum.
+
+payload='{
+  "url": "https://gateway-arbitrum.network.thegraph.com/api/{api-key}/subgraphs/id/GyijYxW9yiSRcEd5u2gfquSvneQKi5QuvU3WZgFyfFSn",
+  "body": {
+    "query": "query Delegate($id: ID!) { delegate(id: $id ) { numberVotes } }",
+    "variables": { "id": "0x534631bcf33bdb069fb20a93d2fdb9e4d4dd42cf" }
+  }
+}'
+
+# Generate a semi-random cache key
+random_cache_key="cachekey_$(date +%s)_$RANDOM"
+
+curl -X POST "http://localhost:8787/$random_cache_key" \
+     -d "$payload"
